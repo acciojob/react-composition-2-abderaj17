2367f8699d91e15c51e4b954466473fda41486d5
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-30 21:17:37 +0530

    com

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,18 @@ import Model from "./Model";
 
 const App = () => {
   const [displayModel, setDisplayModel] = React.useState(false);
-  const handleModel = () => {
-    setDisplayModel(!displayModel);
+  const handleModel = () =>{
+    setDisplayModel(true);
+  }
+
+  const closeModel = () =>{
+    setDisplayModel(false);
   }
   return (
     <div>
      <button style={{padding: '10px', margin: '10px', backgroundColor:"red", borderRadius: '5px'}} className="model-open" onClick={handleModel} >Show Model</button>
 
-     <Model show={displayModel} onClose={() => setDisplayModel(false)}>
+     <Model show={displayModel} onClose={closeModel}>
       This is the content of the model.
      </Model>
     </div>
