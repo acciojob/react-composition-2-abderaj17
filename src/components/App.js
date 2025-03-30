df0ac3833f26071f9818246fb38337bdd7c3a98f
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-30 20:43:27 +0530

    com

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 
 import React from "react";
 import './../styles/App.css';
+import Model from "./Model";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+     <button className="model-open" onClick={<Model />} >Show Model</button>
     </div>
   )
 }
---
+++ src/components/Model.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Model = () => {
+  return (
+    <div>
+        <div className='model-overlay'>
+            <button className='model-close'>Close</button>
+            <p className='model-p'>This is the content of the model.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Model
